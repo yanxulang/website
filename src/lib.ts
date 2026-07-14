ce0cd1321fcff5e183c0dfc536ec8bca6441ff34
刘秀 <xiu@srcmax.com>
2026-07-15 03:55:45 +0800

    chore: 开启 1.1.5 正式下载

--- src/lib.ts
+++ src/lib.ts
@@ -6,7 +6,7 @@ export const docsUrl = "https://docs.yanxu.dev/";
 export const version = "1.1.5";
 export const vscodeVersion = "1.3.1";
 // GitHub Release 工作流会为当前稳定版本生成六个平台资产及校验文件。
-export const binaryDownloadsReady = false;
+export const binaryDownloadsReady = true;
 export const releaseTag = `v${version}`;
 export const releaseUrl = `${repository}/releases`;
 export const releaseDownloadUrl = `${repository}/releases/download/${releaseTag}`;
